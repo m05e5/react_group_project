b7dc915a5f67d24b121758b68c841b0a96efe7d9
m05e5 <calebmoses1111@gmail.com>
2021-09-08 12:14:26 +0100

    rocket booking, reserve and cancel reserve

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,21 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { rockets } from '../redux/rockets/rockets';
+import { rockets, reserveRockets, cancelReservation } from '../redux/rockets/rockets';
 
 const baseUrl = 'https://api.spacexdata.com/v3/rockets';
 
 const Rockets = () => {
-  const state = useSelector((state) => state.rocketsReducer.state);
+  const state = useSelector((state) => state.rocketsReducer);
   const dispatch = useDispatch();
 
   const getRockets = () => {
-    fetch(baseUrl).then((data) => {
-      data.json().then((dataJson) => dispatch(rockets(dataJson)));
-    });
+    if (!state.length > 0) {
+      fetch(baseUrl).then((data) => {
+        data.json().then((dataJson) => dispatch(rockets(dataJson)));
+      });
+    }
+  };
+  const reserve = (id) => {
+    dispatch(reserveRockets(id));
+  };
+  const cancelReserve = (id) => {
+    dispatch(cancelReservation(id));
   };
   useEffect(() => {
     getRockets();
-    console.log(state);
   }, []);
   return (
     <div>
@@ -23,11 +30,14 @@ const Rockets = () => {
         {state.map((rocket) => (
           <li key={rocket.rocket_id}>
             <img src={rocket.flickr_images[0]} alt="rocket" />
+            {rocket.reserved && <p id="reserved">yes yes yes yes yes yes yes</p> }
             <p>{rocket.rocket_id}</p>
             <p>{rocket.rocket_name}</p>
             <p>{rocket.rocket_type}</p>
             <p>{rocket.description}</p>
-            <button type="button">Reserve Rocket</button>
+            {rocket.reserved
+              ? <button type="button" onClick={() => cancelReserve(rocket.id)}>Cancel Reservation</button>
+              : <button type="button" onClick={() => reserve(rocket.id)}>Reserve Rocket</button>}
           </li>
         ))}
       </ul>
